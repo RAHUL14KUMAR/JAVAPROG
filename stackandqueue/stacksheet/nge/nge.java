aae10469e6b930d33624bc316b62f42612b76299
RAHUL14KUMAR <rahul.kumar.it25@heritageit.edu.in>
2024-07-25 18:17:23 +0530

    25/07/2024

--- stackandqueue/stacksheet/nge/nge.java
+++ stackandqueue/stacksheet/nge/nge.java
@@ -4,13 +4,12 @@ public class nge {
         Stack<Integer> st=new Stack<>();
         int n[]={3,10,4,2,1,2,6,1,7,2,9};
 
-        int ans[]=new int[n.length];
-        int a=n[10];
-        st.push(a);
+        int ans[]=new int[11];
+        st.push(n[n.length - 1]);
 
         for(int i=n.length-2;i>=0;i--){
-            if(n[i]>st.peek()){
-                while(!st.isEmpty() && n[i]>st.peek()){
+            if(n[i]>=st.peek()){
+                while(!st.isEmpty() && n[i]>=st.peek()){
                     st.pop();
                 }
                 if(st.isEmpty()){
@@ -21,12 +20,12 @@ public class nge {
             }else{
                 ans[i]=st.peek();
             }
+            st.push(n[i]);
         }
-        if(st.size()>0){
-            ans[n.length-1]=st.pop();
-        }else{
-            ans[n.length-1]=-1;
+        while(st.size()>0 && n[10]>st.peek()){
+            st.pop();
         }
+       ans[n.length-1]=st.pop();
 
         for(int i=0;i<ans.length;i++){
             System.out.print(ans[i]+" ");
---
+++ stackandqueue/stacksheet/nse/nse.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class nse {
+    public static void main(String args[]){
+        Stack<Integer> st=new Stack<>();
+        int a[]={4, 5, 2, 10, 8};
+
+        int ans[]=new int[a.length];
+        st.push(a[0]);
+        ans[0]=-1;
+
+        for(int i=1;i<a.length;i++){
+            if(a[i]<=st.peek()){
+                while(!st.isEmpty() && a[i]<=st.peek()){
+                    st.pop();
+                }
+                if(st.isEmpty()){
+                    ans[i]=-1;
+                }else{
+                    ans[i]=st.peek();
+                }
+            }else{
+                ans[i]=st.peek();
+            }
+            st.push(a[i]);
+        }
+
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
